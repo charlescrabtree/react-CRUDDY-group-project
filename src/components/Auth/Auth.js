1a7860663a3e5bfdd7623ed7ed6e4b3e4843ab00
Connor McCue <connor.mccue@comcast.net>
2022-10-05 14:49:12 -0700

    auth.js v1

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { useUser } from '../context/UserContext';
+import { authUser } from '../services/auth';
+import { Redirect, useParams } from 'react-router-dom';
+
+export default function Auth() {
+  const { type } = useParams();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { user, setUser } = useUser();
+
+  const clickHandler = async () => {
+    const userResp = await authUser(email, password, type);
+    setUser(userResp);
+    setEmail('');
+    setPassword('');
+  };
+
+  if (user) {
+    return <Redirect to="/posts" />;
+  }
+
+  return (
+    <div>
+      <div className="form-controls">
+        <label>Email:</label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div className="form-controls">
+        <label>Password:</label>
+        <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button onClick={clickHandler}>Submit</button>
+    </div>
+  )
+
+
+}
