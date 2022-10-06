b1d26653047d10a223dcbbc9047d15a73c203d47
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-10-06 12:00:08 -0700

    edit button in postcard works

--- src/components/Posts/PostCard.js
+++ src/components/Posts/PostCard.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
 
 export default function PostCard({ title, description, user_id, id }) {
-//   const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
+  const owner = user.id === user_id;
 
   return (
-    <div>
+    <div className="post">
       <h3>{title}</h3>
       <p>{description}</p>
-      <button>only renders if user is detected</button>
+      {owner && (
+        <p>
+          <Link to={`/posts/edit/${id}`}>Edit </Link>
+        </p>
+      )}
     </div>
   );
 }
