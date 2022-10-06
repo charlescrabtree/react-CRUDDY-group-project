5fcfd9a5907404477daf35801d3a86e664dabc81
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-10-06 12:28:52 -0700

    delete button works

--- src/components/Posts/PostCard.js
+++ src/components/Posts/PostCard.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
+import { deletePost } from '../../services/posts';
 
 export default function PostCard({ title, description, user_id, id }) {
   const { user } = useContext(UserContext);
   const owner = user.id === user_id;
 
+  const handleDelete = async () => {
+    try {
+
+      await deletePost(id);
+      history.push('/posts');
+    } catch (e) {
+      //eslint-disable-next-line no-console
+      console.error(e.message);
+    }
+  }; 
   return (
     <div className="post">
       <h3>{title}</h3>
@@ -14,7 +25,7 @@ export default function PostCard({ title, description, user_id, id }) {
       {owner && (
         <p>
           <Link to={`/posts/edit/${id}`}>Edit </Link>
-          <button className="delete">Delete</button>
+          <button className="delete" onClick={handleDelete} >Delete</button>
         </p>
       )}
     </div>
