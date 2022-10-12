9512f1f6ed4a8610a7eee5d79b79eaa836362fdf
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-10-12 12:22:40 -0700

    3 tests pass

--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,10 @@ import { UserProvider } from './context/UserContext';
 const { MemoryRouter } = require('react-router-dom');
 
 import * as authFns from './services/auth';
+import * as postFns from './services/posts';
 
 jest.mock('./services/auth');
+jest.mock('./services/posts');
 
 const mockUser = {
   id: '0dab2c65-5911-469c-9f12-8fb47ebe52f2',
@@ -50,4 +52,25 @@ test('user can sign in', async () => {
   const button = screen.getByRole('button');
   fireEvent.click(button);
 
+});
+
+const fakePosts = [
+  { id: 1, title: 'This is a fake post #1', description: 'This is a fake description #1' },
+  { id: 2, title: 'This is also a fake post #2', description: 'This is a fake description #2' },
+];
+
+test('posts display', async () => {
+  authFns.getUser.mockReturnValue(mockUser);
+  postFns.getPosts.mockReturnValue(fakePosts);
+
+  render(
+    <UserProvider>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </UserProvider>
+  );
+
+  await screen.findByText(/fake post #1/i);
+  await screen.findByText(/fake post #2/i);
 });
